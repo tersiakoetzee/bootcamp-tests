05fd337ec01cefb0bd72b79f67cebf3b484e6923
Tersia Koetzee <tersiakoetzee@gmail.com>
2019-05-16 10:56:34 +0200

    changes made to the most profitable department

--- mostProfitableDepartment.js
+++ mostProfitableDepartment.js
@@ -1,39 +1,44 @@
-function mostProfitableDepartment(salesData){
-    var successfulDept = "";
-    var highestSale = 0;
-    for (var i in salesData){
-      var aSalesEntry = salesData[i];
-      var currentSale = aSalesEntry.sales;
-  
-      var currentDepartment = aSalesEntry.department;
-  
-      if (currentSale > highestSale){
-        highestSale = currentSale;
-        successfulDept = currentDepartment;
-  
-      }
+function mostProfitableDepartment(salesData) {
+  var successfulDept = "";
+  var highestSale = 0;
+  for (var i in salesData) {
+    var aSalesEntry = salesData[i];
+    var currentSale = aSalesEntry.sales;
+
+    var currentDepartment = aSalesEntry.department;
+
+    if (currentSale > highestSale) {
+      highestSale = currentSale;
+      successfulDept = currentDepartment;
+
     }
-    return successfulDept;
   }
-  
-  function mostProfitableDay(data){
-    var wednesdaySum = 0;
-    var wednesdaySumArray = [];
-    var compare = 0;
-    var successfulDept = "";
-    var highestSale = 0;
-    for (var i in data){
-      var aSalesEntry = data[i];
-      var currentSale = aSalesEntry.sales;
-      var currentDay = aSalesEntry.day;
-  
-      if (currentDay.startsWith("W")){
-       wednesdaySum += currentSale
-        wednesdaySumArray.push(wednesdaySum);
-        //console.log(wednesdaySumArray);
-       for (var i=0;i<wednesdaySumArray.length;i++){
-  var aRow = wednesdaySumArray[i];
-         console.log(aRow);}
-        }
+  return successfulDept;
+}
+
+function mostProfitableDay(data) {
+  var thursdaySum = 0;
+  var thursdaySumArray = [];
+  var compare = 0;
+  var successfulDept = "";
+  var highestSale = 0;
+  for (var i in data) {
+    var aSalesEntry = data[i];
+    var currentSale = aSalesEntry.sales;
+    var currentDay = aSalesEntry.day;
+
+    if (currentDay.startsWith("T")) {
+      thursdaySum += currentSale
+      thursdaySumArray.push(thursdaySum);
+
+      for (var i = 0; i < thursdaySumArray.length; i++) {
+        var aRow = thursdaySumArray[i];
+        console.log(aRow);
+
+
+      }
+
+
     }
+  }
 }
--- test/mostProfitableDepartment.test.js
+++ test/mostProfitableDepartment.test.js
@@ -1,14 +1,11 @@
 describe("the mostProfitableDepartment function", function () {
 
-    it("Should return true for the most profitable department ", function () {
-        assert.equal(true, mostProfitableDepartment("highestSale outdoor sales Thursday"), 18007 );
-    });
-    it("should return true for the most profitable department", function () {
-        assert.equal(true, mostProfitableDepartment("highestSale carpentry, sales", 16109, "Friday"),);
+    it("Should return  the most profitable department ", function () {
+        assert.deepEqual(mostProfitableDepartment([{"department":"hardware","sales":4500,"day":"Monday"},{"department":"outdoor","sales":1500,"day":"Monday"},{"department":"carpentry","sales":5500,"day":"Monday"},{"department":"hardware","sales":7500,"day":"Tuesday"}]), "hardware");
     })
 
     it("should return false for the least profitable department", function () {
-        assert.equal(false, mostProfitableDepartment("leastProfitable hardware sales 1500 Wednesday"), );
+        assert.equal(false, mostProfitableDepartment("hardware department"));
 
     })
-})
+}) 
